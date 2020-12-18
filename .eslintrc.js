15626459b7f767551494a5ea6c622cb3b663b751
Philipp Fritsche <ph.fritsche@gmail.com>
2020-12-18 13:47:39 +0100

    Update eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,5 @@
 module.exports = {
     extends: [
-        '@ph-fritsche/eslint-config-react',
-    ],
-    overrides: [
-        {
-            files: ['**.{ts,tsx}'],
-            parser: '@typescript-eslint/parser',
-            plugins: [
-                '@typescript-eslint',
-            ],
-            extends: [
-                '@ph-fritsche/eslint-config-react',
-                'plugin:@typescript-eslint/recommended',
-            ],
-        },
+        '@ph.fritsche/eslint-config',
     ],
 }
