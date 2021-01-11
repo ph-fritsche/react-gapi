37ec2c917e412bc50128e264c9fdb699434508cf
Philipp Fritsche <ph.fritsche@gmail.com>
2021-01-11 17:29:39 +0100

    chore: bump deps

--- src/react-weblibrary.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-weblibrary' {
-    type status = 'try' | 'error' | 'load'
-
-    export function useLibrary(name: string, url: string): [library: unknown, status: status, tryAgain: () => void];
-}
